4b69ce0bf5276ac2da10feb07c4c63f8af898f2a
OJ101003 <hackerdude1010@gmail.com>
2024-03-01 21:17:48 -0800

    Added status buttons

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   var selectedIndex = 0;
 
   @override
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -8,6 +8,22 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  var currentStatus = "Current Status";
+  final List<String> buttonLabels = [
+    'Button 1',
+    'Button 2',
+    'Button 3',
+    'Button 1',
+    'Button 2',
+    'Button 3',
+    'Button 1',
+    'Button 2',
+    'Button 3',
+    'Button 1',
+    'Button 2',
+    // Add as many button labels as you need
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,14 +63,21 @@ class _ProfilePageState extends State<ProfilePage> {
                         // Handle the button press
                       },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF6452AE), // Button background color
-                        foregroundColor: Colors.white, // Text color
-                        elevation: 5, // Button shadow elevation
+                        backgroundColor: const Color(0xFF6452AE),
+                        // Button background color
+                        foregroundColor: Colors.white,
+                        // Text color
+                        elevation: 5,
+                        // Button shadow elevation
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30), // Rounded shape
+                          borderRadius:
+                              BorderRadius.circular(30), // Rounded shape
                         ),
-                        minimumSize: const Size(150, 40), // Set the button's minimum size
-                        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10), // Inner padding of the button
+                        minimumSize: const Size(150, 40),
+                        // Set the button's minimum size
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25,
+                            vertical: 10), // Inner padding of the button
                       ),
                       child: const Text(
                         'Edit Profile',
@@ -70,12 +93,128 @@ class _ProfilePageState extends State<ProfilePage> {
             ],
           ),
         ), // Top part with pfp, username, and edit profile button
-        const Row(), // Row with create status and current status
+        Container(
+          margin: const EdgeInsets.only(top: 20, left: 7, right: 7, bottom: 10),
+          child: Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Handle the button press
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF6452AE),
+                  // Button background color
+                  foregroundColor: Colors.white,
+                  // Text color
+                  elevation: 5,
+                  // Button shadow elevation
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30), // Rounded shape
+                  ),
+                  minimumSize: const Size(140, 75),
+                  // Set the button's minimum size
+                  // Inner padding of the button
+                ),
+                child: const Text(
+                  'Create Status',
+                  style: TextStyle(
+                    fontSize: 20, // Set the font size
+                    fontWeight: FontWeight.bold, // Set the font weight
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 5),
+                  height: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.black),
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: Center(
+                    child: Text(
+                      currentStatus,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ), // Row with create status and current status
         Expanded(
-            child: ListView(
-                // This is where the list of all the buttons will go, each button row will be it's own widget
-                ))
+          child: ListView.builder(
+            itemCount: (buttonLabels.length / 2).ceil(),
+            // Calculate the number of rows needed
+            itemBuilder: (context, index) {
+              int firstButtonIndex = index * 2;
+              List<Widget> rowChildren = [
+                CustomButton(
+                  label: buttonLabels[firstButtonIndex],
+                  onPressed: () {
+                    setState(() {
+                      currentStatus = buttonLabels[firstButtonIndex];
+                    });
+                  },
+                ),
+              ];
+
+              int secondButtonIndex = firstButtonIndex + 1;
+              if (secondButtonIndex < buttonLabels.length) {
+                rowChildren.add(
+                  CustomButton(
+                    label: buttonLabels[secondButtonIndex],
+                    onPressed: () {
+                      setState(() {
+                        currentStatus = buttonLabels[secondButtonIndex];
+                      });
+                    },
+                  ),
+                );
+              }
+
+              return Row(children: rowChildren);
+            },
+          ),
+        )
       ],
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFFD9D9D9), // Button background color
+            foregroundColor: Colors.white, // Text color
+            elevation: 5, // Button shadow elevation
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30), // Rounded shape
+            ),
+            minimumSize: const Size(140, 75), // Set the button's minimum size
+          ),
+          child: Text(
+            label,
+            style: const TextStyle(color: Colors.black, fontSize: 18),
+          ),
+        ),
+      ),
     );
   }
 }
