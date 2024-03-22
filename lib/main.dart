566359121497dcb0672bbf70b69a555d5d485244
OJ101003 <hackerdude1010@gmail.com>
2024-03-21 22:18:54 -0700

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:now_me/add_friend.dart';
 import 'package:now_me/profile_page.dart';
@@ -8,7 +8,9 @@ import 'package:now_me/register_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
